7c5178c3c35db133a4d5f4c9c134dcfcfbda30c5
Marius CA <marius@example.com>
2023-03-08 11:27:06 +0200

    render

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 import { getData } from './modules/fetch.js';
+import { render } from './modules/generate.js';
 
 const prodUrl = 'https://dummyjson.com/products';
+
+// taikomes
+export const shopItemsList = document.getElementById('shopItemsList');
 
 async function app() {
   let mainShopItemsArr = [];
   console.log('app started');
-
+  // gaunam produktus
   const { products } = await getData(prodUrl);
   console.log('products ===', products);
   mainShopItemsArr = products;
+  // sugeneruojam sarasa
+  render(mainShopItemsArr);
 }
 
 app();
--- js/modules/generate.js
+++ js/modules/generate.js
@@ -1,3 +1,4 @@
+import { shopItemsList } from '../main.js';
 /*
 <li class="shopItem">
   <div class="shopItemTop">
@@ -12,7 +13,26 @@
 </li>
 */
 
-function makeOneItem(shopItemObj) {
+export function makeOneItem(shopItemObj) {
   // sukurti isorini li create element
+  const liEl = document.createElement('li');
+  liEl.className = 'shopItem';
   // vidu sukurti su innerHtml
+  liEl.innerHTML = `
+    <div class="shopItemTop">
+      <img src="/img/prod-1.png" alt="item image">
+      <span>sale</span>
+    </div>
+    <div class="shopItemInfo">
+      <h3 class="title">Yellow Reserved Hoodie</h3>
+      <p class="category">Dress</p>
+      <p class="price onSale"><span class="salePrice">$364.00</span>$155.00</p>
+    </div>
+  `;
+  return liEl;
+}
+
+export function render(arr) {
+  shopItemsList.innerHTML = '';
+  arr.map(makeOneItem).forEach((liEl) => shopItemsList.append(liEl));
 }
